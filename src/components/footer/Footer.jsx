9be3ad7d13bf7b19082f586f674c30f489cf9e2c
ozodbec <151632769+ozodbec@users.noreply.github.com>
2024-06-18 17:37:16 +0500

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+// style
+import './Footer.css'
+
+
+function Footer() {
+  return (
+    <div className='footer'>
+     <div className='footer-container container'>
+      <p>All Right Reserved. <a href="https://t.me/ozodbekweb">Мирзаахмадов</a></p>
+     </div>
+    </div>
+  )
+}
+
+export default Footer;
+
